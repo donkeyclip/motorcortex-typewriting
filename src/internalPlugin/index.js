80a63f573bf7e2fdabb5ade334044d544ddcb8fb
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2022-03-11 12:52:09 +0200

    fix: demo and code structure

---
+++ src/internalPlugin/index.js
@@ -0,0 +1,12 @@
+import TypeWritingIncident from "./TypeWritingIncident";
+export default {
+  npm_name: "type-writing-definition",
+  version: "1.0.0",
+  incidents: [
+    {
+      exportable: TypeWritingIncident,
+      name: "TypeWritingIncident",
+      attributesValidationRules: {},
+    },
+  ],
+};
